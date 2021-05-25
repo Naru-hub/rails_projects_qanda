da0ce35f4553a9c96b35d41ffc4ebdf68c4342ea
Naru-hub <1208ny0324@gmail.com>
2021-05-25 05:05:49 +0000

    qanda

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,31 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+
+  end
+
+  def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: 'Success!'
+    else
+      flash[:alert] = 'Save error!'
+    end
+  end
+
+  def edit
+  end
+end
+
+private
+
+def question_params
+  params.require(:question).permit(:name, :title, :content)
+end
