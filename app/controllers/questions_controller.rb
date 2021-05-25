6daa38b9bd1fa832b3cfe643781ac151225eb66c
Naru-hub <1208ny0324@gmail.com>
2021-05-25 13:19:12 +0000

    add answer form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,11 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.all
-
   end
 
   def show
     @question = Question.find(params[:id])
+    @answer = Answer.new
   end
 
   def new
